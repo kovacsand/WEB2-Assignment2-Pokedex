80786e0485b4228c3c5a66e711ff5b229cb4d009
András Kovács <k.andras9@gmail.com>
2023-04-09 13:52:31 +0200

    testing the api, and basic react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [pokemon, setPokemon] = React.useState(null);
+  const updatePokemon = (newPokemon) => {
+    setPokemon(newPokemon);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Current Pokemon: {pokemon?.name}</h2>
+        <GetPokemonButton callback={updatePokemon}/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function fetchPokemon({name, callback}) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response => {
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json();
+    })
+    .then(pokemon => callback(pokemon))
+    .catch(error => console.log("Error: " + error))
+}
+
+function GetPokemonButton({callback}) {
+  return (
+    <>
+      <input type="text" id="inputName"/>
+      <button 
+        onClick={ () => {
+            const name = document.getElementById("inputName").value;
+            fetchPokemon({name, callback})
+          }
+        }
+      >
+        Update Pokemon
+      </button>
+    </>
+  )
+}
