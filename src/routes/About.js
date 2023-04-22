320f0b056b4359ee6e743230b6d28b237d5952d3
Andras Kovacs <47793048+kovacsand@users.noreply.github.com>
2023-04-22 22:37:55 +0200

    source code added

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function About() {
+	return (
+		<div>
+			<Link to="/">Back</Link>
+			<div>This Kanto Pokédex was created for the WEB2 course.</div>
+		</div>
+	);
+}
